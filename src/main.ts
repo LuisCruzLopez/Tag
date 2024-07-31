6146d839505660768cf027a848875770b976408f
Luis Fernando de la Cruz Lopez <cruzfernando301@hotmail.com>
2024-07-31 14:49:56 -0600

    first commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+// import ComponenetMultiselect from 'vue-multiselect'
+// import 'vue-multiselect/dist/vue-multiselect.min.css'
+
+const app = createApp(App)
+// app.component('ComponenetMultiselect', ComponenetMultiselect)
+app.mount('#app')
